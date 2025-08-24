c928ac8a15c98458ac36d7cddbf997f852188601
k1a2t3h4 <kathiravankumar2004@gmail.com>
2025-08-25 00:25:38 +0530

    productsection in completed

--- src/components/contexts/ProductContext.tsx
+++ src/components/contexts/ProductContext.tsx
@@ -491,8 +491,12 @@ const handleComboMediaDragEnd = () => {
   };
   
   const updateVariantName = (id: string, name: string) => {
+    if(productFormData().variantOptions?.map(variant =>
+      variant.name === name && variant.id !== id)){
+        return
+      }
     const updated = productFormData().variantOptions?.map(variant =>
-      variant.id === id  ? { ...variant, name } : variant
+      variant.id === id ? { ...variant, name } : variant
     );
     generateSmartVariantCombinations(updated!, productFormData().variantCombinations??[]);
     setProductFormData({ ...productFormData(), variantOptions: updated });
